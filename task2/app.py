e66f582c2c45012af5161703997c537773d62667
unknown <ghadeerdarwesh32@gmail.com>
2020-04-07 00:14:03 +0300

    Create app python file

---
+++ task2/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+       return render_template('index.html')
+
+
+@app.route('/img/<path:path>')
+def index2(path):
+       return send_from_directory('static\\images',path)
+       
+@app.route('/static/<path:path>')
+def index3(path):
+     return app.send_static_file(path)
+        
+if __name__ == '__main__':
+
+    #app.run(host='127.0.0.1',port='5000',debug = True,use_reloader=False)
+    app.run(threaded=True, port='5000')
+
